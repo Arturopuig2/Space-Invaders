924c2a832658ef030d370a1492e376775368d43c
Arturo Puig <arturopuig2@gmail.com>
2022-01-22 21:03:53 +0100

    Ejercicio 2

--- Assets/Scripts/SpaceShipScript.cs
+++ Assets/Scripts/SpaceShipScript.cs
@@ -8,14 +8,16 @@ public class SpaceShipScript : MonoBehaviour
     Rigidbody2D myRB;
     public int force;
     public int forceTorpedo;
-    public GameObject bala;
     public GameObject torpedo;
+    public GameObject clone;
+    public float TiempoDeVida=1f;
 
     // Start is called before the first frame update
     void Start()
     {
-       myRB=GetComponent<Rigidbody2D>();
-        
+        myRB =GetComponent<Rigidbody2D>();
+
+       // Instantiate(torpedo, transform.position, Quaternion.identity);
     }
     
     // Update is called once per frame
@@ -23,28 +25,31 @@ public class SpaceShipScript : MonoBehaviour
     {
         float movement = Input.GetAxis("Horizontal");
         myRB.velocity=(transform.right*movement*force);
-
-
         float xPos = Mathf.Clamp(myRB.position.x, -3f, 3f);
-
         myRB.position = new Vector2(xPos, myRB.position.y);
 
-        if (Input.GetButton("Jump"))
-        {
-            Debug.Log("he pulsado disparo");
-        }
-            if (Input.GetButton("Jump"))
-            {
 
+        if (Input.GetButton("Jump"))
+            {
+            Debug.Log("He pulsado disparo");
             Vector2 posTorpedo = new Vector2(transform.position.x, transform.position.y+1.5f);
 
-           GameObject clone= Instantiate(torpedo, posTorpedo, Quaternion.identity);
 
-            Rigidbody2D cloneRB = clone.GetComponent<Rigidbody2D>();
+            if (!clone)
+            {
+                clone = Instantiate(torpedo, posTorpedo, Quaternion.identity); //Creamos un GO que es la instanciación del Torpedo.
+                Rigidbody2D cloneRB = clone.GetComponent<Rigidbody2D>(); //Creamos una variable de tipo Rigidbody2D con las cualidades del RB del clone.
+                Vector2 direccion = new Vector2(0f, 1f);
+                cloneRB.velocity = direccion * forceTorpedo;
+                Destroy(clone, TiempoDeVida);
+            }
 
-            Vector2 direccion = new Vector2(0f,1f);
-
-            cloneRB.velocity=direccion * forceTorpedo;
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("LA NAVE ENTRA AL ÁREA");
+    }
+
 }
---
+++ Assets/Scripts/torpedo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class torpedo : MonoBehaviour
+{
+
+    public float livingTime = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  Destroy(this.gameObject, livingTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
