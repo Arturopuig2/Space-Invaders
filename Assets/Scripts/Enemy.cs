65c34e0a513911073563fbcdf206d318262f1a42
Arturo Puig <arturopuig2@gmail.com>
2022-01-30 20:39:10 +0100

    Ejercicio 4

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
-    public int enemyForce; //Velocidad a la que se desplaza.
+    public float enemyForce=2f; //Velocidad a la que se desplaza.
     Rigidbody2D enemyRB;
     public int direccion=1;
     public float enemyPosition;
-
+    //public float enemyPositionY;
 
     // Start is called before the first frame update
     void Start()
@@ -28,15 +28,23 @@ public class Enemy : MonoBehaviour
         Debug.Log(enemyPosition);
         enemyRB.velocity = (transform.right*direccion* enemyForce); //Se mueve a la dcha. Si quiero a la izq. tengo que darle -1
 
-        if (enemyPosition>5)
+        if (enemyPosition>=5.00)
         {
             Debug.Log("SOY MAYOR QUE 5");
+
             direccion = -1;
+            enemyForce = enemyForce + 0.02f;
+            transform.Translate(Vector3.down*0.2f);                
+
         }
-        else if(enemyPosition<-5)
+        else if(enemyPosition<=-5.00)
         {
             Debug.Log("SOY MENOR QUE 5");
             direccion = +1;
+            enemyForce = enemyForce + 0.02f;
+            transform.Translate(Vector3.down * 0.2f);
+
+
         }
 
 
